e1eb239791d3e1c52e7e9e86372d42858b56c2f1
yk1028 <wongeunsong94@gmail.com>
2018-12-09 17:42:59 +0900

    Initial Commit

---
+++ MyBoxOffice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyBoxOffice
+//
+//  Created by Wongeun Song on 2018. 12. 9..
+//  Copyright © 2018년 Wongeun Song. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
